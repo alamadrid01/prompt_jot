3892565713c7987ea0354d1d8b393165cb1540ed
Al-ameen <adebayoalameen88@gmail.com>
2024-07-06 12:03:46 +0100

    test

--- web-app/src/app/api/v1/test/route.ts
+++ web-app/src/app/api/v1/test/route.ts
@@ -1,31 +1,35 @@
 import { NextResponse } from "next/server"
 
-
 export async function GET(request: any){
     const name = await request.nextUrl.searchParams.get("visitor_name");
-    const city = await request.geo.city
-    const country = await request.geo.country
-    const region = await request.geo.region
-    const latitude = await request.geo.latitude
-    const longitude = await request.geo.longitude
-    const ip = await request.headers.get('X-Forwarded-For') || request.headers.get('CF-Connecting-IP') || request.headers.get('Fastly-Client-IP') || request.headers.get('True-Client')
-
-    // console.log(city, country, region, latitude, longitude, ip)
-
-    
-
     if(!name){
         return NextResponse.json({message: "Please provide a name"}, {status: 400})
     }
 
-    return NextResponse.json({
-        client_ip: `${ip}`,
-        location: "Lagos, Nigeria",
-        greeting: `Hello ${name}! the temperature is 30 degrees Celsius`,
-        city: `${city}`,
-        country: `${country}`,
-        region: `${region}`,
-        latitude: `${latitude}`,
-        longitude: `${longitude}`
-    }, {status: 200 })
+
+    const ip = await request.headers.get('X-Forwarded-For') || request.headers.get('CF-Connecting-IP') || request.headers.get('Fastly-Client-IP') || request.headers.get('True-Client')
+
+    try{
+        const getData = await fetch(`https://freeipapi.com/api/json/${ip}`)
+        const data = await getData.json()
+        const city = data.regionName
+        const country = data.countryName
+
+        const getWeather = await fetch(`http://api.weatherapi.com/v1/current.json?q=${city}&key=${process.env.WEATHER}`)
+        const weather = await getWeather.json()
+        const temperature = weather.current.temp_c
+
+        return NextResponse.json({
+            client_ip: `${ip}`,
+            location:  `You are in ${city}, ${country}`,
+            greeting: `Hello ${name}! the temperature is ${temperature} degrees Celsius`,
+        }, {status: 200 })
+
+    }catch(err: any){
+        new Error(err.message)
+    }
+
+    // console.log(city, country, region, latitude, longitude, ip)
+
+
 }
